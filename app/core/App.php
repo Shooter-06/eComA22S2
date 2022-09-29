d1a98b42b224e953f6d2a5537238d54fe486ab76
Shooter-06 <john190curry@gmail.com>
2022-09-28 21:56:24 -0400

    Model

--- app/core/App.php
+++ app/core/App.php
@@ -2,28 +2,28 @@
 namespace app\core;
 
 class App{
-	private $controller ='Main';
-	private $method ='index';
+	private $controller = 'Main';
+	private $method = 'index';
 
 	public function __construct(){
 		//echo $_GET['url'];
 		//TODO: replace this echo with the routing algorithm
 		//goal: separate the url in parts
 
-		$url =self::parse_url(); //get the url parsed and returned as an array of URL segment
-
-		//use the first part to determine if the controller class to load
+		$url = self::parseUrl(); //get the url parsed and returned as an array of URL segment
+		
+		//use the first part to determine the controller class to load
 
 		if(isset($url[0])){
-			if(file_exists('app/controllers/' . $url[0] .'.php')){
+			if(file_exists('app/controllers/' . $url[0] . '.php')){
 				$this->controller = $url[0]; //$this refers to the current object
 			}
 			unset($url[0]);
 		}
-		$this->controller ='app\\controllers\\' . $this->controller; 
-		$this->controller= new $this->controller; 
+		$this->controller = 'app\\controllers\\' . $this->controller; //provide a fully qualified classname
+		$this->controller = new $this->controller;
 
-		//provide fully qualified classname
+		//use the second part to determine the method to run
 
 		if(isset($url[1])){
 			if(method_exists($this->controller, $url[1])){
@@ -34,19 +34,19 @@ class App{
 
 		//...while passing all other parts as arguments
 		//repackage the parameters
-		$params = $url ? array_values($url):[];
-
-		call_user_func_array([$this->controller, $this->method], $params);
+		$params = $url ? array_values($url) : [];
+		call_user_func_array([ $this->controller, $this->method ], $params);
 	}
 
-	public static function parse_url(){
+	public static function parseUrl(){
 		if(isset($_GET['url']))//get url exists
 		{
 			return explode('/', //return parts in an array, separated by /
-				filter_var( //remove non-URL characters and sequences 
+				filter_var(	//remove non-URL characters and sequences
 					rtrim($_GET['url'], '/'))
 				,FILTER_SANITIZE_URL);
 		}
-
 	}
+
+
 }
--- app/core/Controller.php
+++ app/core/Controller.php
@@ -2,9 +2,19 @@
 namespace app\core;
 
 class Controller{
-//TODO: add dafa to display with the view	
-	protected function view ($name, $data=[]){
-		//call in a view to display 
-		include('app\\views\\' . $name .'.php');
+//TODO: add a parameter for data later
+	public function view($name, $data = []){
+		include('app/views/' . $name . '.php');
+	}
+
+	public function saveFile($file){
+		$check = getimagesize($file['tmp_name']);
+		$allowed_types = ['image/png'=>'png', 'image/jpeg'=>'jpg'];
+		if(in_array($check['mime'], array_keys($allowed_types))){
+			$ext = $allowed_types[$check['mime']];
+			$filename = uniqid() . "." . $ext;
+			move_uploaded_file($file['tmp_name'], 'images/' . $filename);
+		}
+		return $filename;
 	}
 }
