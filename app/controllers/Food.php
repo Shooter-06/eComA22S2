3097ef4b0f5097520feebd8fdcdb39d27c40da4d
Shooter-06 <john190curry@gmail.com>
2022-10-21 21:39:15 -0400

    Food.php CRUD update

--- app/controllers/Food.php
+++ app/controllers/Food.php
@@ -2,13 +2,45 @@
 namespace app\controllers;
 
 class Food{
+	
+	public function index(){
+		//process the form if it is submitted
+		if(isset($_POST['action'])){
+			//create a food object
+			$newfood = new \app\models\Food();
+			//populate the Food object
+			$newfood->name =$_POST['new_food'];
+			//call insert
+			$newfood->insert();
+		}
+
+		//read the food.txt file into a variable
+		$food = new \app\models\Food();
+		$foods = $food->getAll();
+
+		//pass the foods to the view for render and output
+		$this->view('Food/index', $foods);
+	}
+
 
 	public function delete($food_id){//delete a food item here
 		//I would like to delete the record with a specific id
 		$food = new \app\models\Food();
 		$food->deleteAt($food_id);
 		//redirect to the list
-		header('location:/Main/foods');
+		header('location:/Food');
 	}
 
+	public function outputJSON(){
+		//service that outputs JSON 
+		//read the foods.txt file into a variable 
+		$food = new \app\models\Food();
+		$foods =$food->getAll();
+
+		echo json_encode($foods);
+	}
+
+	public function display(){
+		$this->view('Food/display')
+	}
 }
