0e030dc7a744fc7dea11d3a1c56281cf5b15f6c0
Shooter-06 <john190curry@gmail.com>
2022-10-31 10:33:02 -0400

    views updated

--- app/views/User/register.php
+++ app/views/User/register.php
@@ -1,16 +1,27 @@
 <html>
 <head>
-	<title>Edit client</title>
+	<title>Register a new user</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </head>
 
 <body>
+
+<?php
+	if(isset($_GET['error'])){ ?>
+<div class="alert alert-danger" role="alert">
+  <?= $_GET['error'] ?>
+</div>
+<?php	}
+?>
+
+
+
 <form action='' method='post'>
-	<label>First name:<input type="text" name="first_name"/></label><br>
-	<label>Last name:<input type="text" name="last_name" /></label><br>
-	<label>Contact:<input type="text" name="contact" /></label><br>
-	<input type="submit" name="action" value="Save changes" />
+	<label>Username:<input type="text" name="username" /></label><br>
+	<label>Password:<input type="password" name="password" /></label><br>
+	<label>Password confirmation:<input type="password" name="password_confirmation" /></label><br>
+	<input type="submit" name="action" value="Register" />
 </form>
 
 </body>
